1f8d3b2700402455f07a1da194dd963eb79b5f0a
Ravi Gahlla <ravigahlla@gmail.com>
2017-05-14 20:59:37 -0700

    created flashcard front view with label & textfield

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    // MARK: Properties
+    @IBOutlet weak var fcQuestionLabel: UILabel!
+    @IBOutlet weak var fcQuestionTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        fcQuestionTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Disable the Save button while editing.
+    }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        fcQuestionLabel.text = textField.text
+    }
 
 }
 
