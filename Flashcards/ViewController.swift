513bc916d344417f82d9c822affa6281bf07bf7b
Ravi Gahlla <ravigahlla@gmail.com>
2017-05-14 20:49:17 -0700

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Ravi Gahlla on 5/14/17.
+//  Copyright © 2017 Ravi Gahlla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
